a7ccf8b2281949d68cd9bc3ed9829b92a2d819f0
Conor R Shea <cshea@lh-cslab-010.sandiego.edu>
2019-02-06 15:38:10 -0800

    added files from 280 code and started to edit tosh.c

--- comp-310-project01-starter/tosh.c
+++ comp-310-project01-starter/tosh.c
@@ -10,18 +10,34 @@
 #include <unistd.h>
 #include <sys/types.h> 
 #include <sys/wait.h>
-#include <readline/readline.h>
+#include <signal.h>
+//#include <readline/readline.h>
 
+#include "parse_args.h"
+#include "history_queue.h"
 
 // TODO: add your function prototypes here as necessary
-
+void child_reaper(__attribute__ ((unused)) int sig_num) {
+	while (waitpid(-1, NULL, WNOHANG) > 0);
+}
 
 int main(){ 
-
-	// TODO: add a call to signal to register your signal handler on 
-	//       SIGCHLD here
+	char cmdline[MAXLINE];
+	char *argv[MAXARGS];
+	 
+	//register a signal handler for SIGCHLD here
+	struct sigaction sa;
+	sa.sa_handler = child_reaper;
+	sa.sa_flags = 0;
+	sigaction(SIGCHILD, &sa, NULL);
+	
 
 	while(1) {
+
+		// (0) print the shell prompt
+		fprintf(stdout, "tosh> ");
+		fflush(stdout);
+
 		// (1) read in the next command entered by the user
 		char *cmdline = readline("tosh$ ");
 
